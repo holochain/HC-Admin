c8145b84f96b431bbcdafae613f4ee61afadabaa
zo-el <joelulahanna@gmail.com>
2019-01-22 14:39:53 -0600

    deleating node modules

--- node_modules/@storybook/addon-storyshots/stories/storyshot.shallow.test.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import initStoryshots, { shallowSnapshot } from '../src';
-
-initStoryshots({
-  framework: 'react',
-  configPath: path.join(__dirname, '..', '.storybook'),
-  test: shallowSnapshot,
-});
