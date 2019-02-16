267548bc05769cfaee7ad1b07f994cd3349c6734
Connor Turland <connorturland@gmail.com>
2019-02-16 13:05:34 -0500

    fix jdenticon

--- app/components/page-components/Jdenticon.jsx
+++ app/components/page-components/Jdenticon.jsx
@@ -6,31 +6,11 @@ import styles from '../styles/page-styles/DefaultPageMuiStyles'
 import jdenticon from 'jdenticon'
 
 class Jdenticon extends React.Component{
-  el = null
-  componentDidUpdate() {
-    // window.jdenticon.update(this.el)
-  }
-
-  componentDidMount() {
-    const size = 50;
-    const value = this.props.hash;
-    const image = jdenticon(value, size);
-  }
 
   render () {
-    const { hash } = this.props
-    return <svg
-      {...this.props}
-      style={{ verticalAlign: 'middle' }}
-      ref={el => this.handleRef(el)}
-      width="50px"
-      height="50px"
-      data-jdenticon-value={hash}
-      />
-  }
-
-  handleRef (el) {
-    this.el = el
+    const size = 50;
+    const __html = jdenticon.toSvg(this.props.hash, size);
+    return <div dangerouslySetInnerHTML={{__html}} />
   }
 }
 
